9686f307704983044c92857af5219d2688664b3d
sidewinder2020 <sdwinder1@gmail.com>
2017-07-26 09:29:23 -0600

    Create methods for activity class

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,22 @@
+require 'pry'
+
 class Activity
+attr_reader :name, :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, total_cost)
+    @participants[name] = total_cost
+  end
+
+  def total_cost
+    total = @participants.values.reduce do |sum, cost|
+      sum += cost
+    end
+    total
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,47 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new("Brunch")
+
+    assert_equal "Brunch", activity.name
+  end
+
+  def test_it_has_no_participants
+    activity = Activity.new("Brunch")
+
+    assert_equal ({}), activity.participants
+  end
+
+  def test_it_can_add_a_participant
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Jim", 20)
+
+    assert_equal ({"Jim" => 20}), activity.participants
+  end
+
+  def test_its_total_cost
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Jim", 20)
+
+    assert_equal 20, activity.total_cost
+  end
+
+  def test_it_can_add_another_participant
+    activity = Activity.new("Brunch")
+
+    activity.add_participant("Jim", 20)
+    activity.add_participant("Joe", 40)
+
+    assert_equal ({"Jim" => 20, "Joe" => 40}), activity.participants
+  end
 end
